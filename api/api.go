7b572a9ff238a31d2607972495f74c0f3fb0796a
briheet01 <briheetyadav@gmail.com>
2024-04-15 15:22:00 +0530

    removed user
    removed user for now and added in api.go for more ease ( for now )

--- api/api.go
+++ api/api.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -17,13 +19,35 @@ func NewApiServer(port string) *ApiServer {
 	}
 }
 
+type User struct {
+	ID        int64     `json:"id"`
+	Topic     string    `json:"topic"`
+	Key       string    `json:"key"`
+	Value     string    `json:"value"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
 func (s *ApiServer) Serve() {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
 
-	user := &User{}
-	user.RegisterRoute(*subrouter)
+	subrouter.HandleFunc("/user", s.HandleUserRegister).Methods("POST")
 
 	log.Printf("starting the api at %s", s.port)
 	log.Fatal(http.ListenAndServe(s.port, subrouter))
+}
+
+func (s *ApiServer) HandleUserRegister(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+
+	println("in HandleUserRegister")
+	// err := json.NewDecoder(r.Body).Decode(&newUser)
+	// if err != nil {
+	// http.Error(w, err.Error(), http.StatusBadRequest)
+	// return
+	// }
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
 }
