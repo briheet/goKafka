973a5f99ea6c4957081201accc3294f2cccece94
briheet01 <briheetyadav@gmail.com>
2024-04-16 23:11:09 +0530

    producer

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/briheet/goKafka
 
 go 1.22.2
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/confluentinc/confluent-kafka-go v1.9.2
--- producer/producerapi.go
+++ producer/producerapi.go
@@ -1,1 +1,59 @@
 package producer
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func newKafkaProducer() (*kafka.Producer, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
+	if err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func main() {
+	producer, err := newKafkaProducer()
+	if err != nil {
+		fmt.Printf("Failed to create Kafka producer: %s\n", err)
+		return
+	}
+	defer producer.Close()
+
+	topic := "your-topic"
+	message := "Hello, Kafka!"
+
+	err = produceMessage(producer, topic, message)
+	if err != nil {
+		fmt.Printf("Failed to produce message: %s\n", err)
+		return
+	}
+}
+
+func produceMessage(producer *kafka.Producer, topic string, message string) error {
+	deliveryChan := make(chan kafka.Event)
+
+	err := producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          []byte(message),
+	}, deliveryChan)
+	if err != nil {
+		return err
+	}
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		return m.TopicPartition.Error
+	}
+
+	fmt.Printf("Message '%s' delivered to topic %s [%d] at offset %v\n",
+		message, *m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+
+	close(deliveryChan)
+
+	return nil
+}
